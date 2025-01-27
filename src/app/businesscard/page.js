331d45457745f593da3a46597c78e42ddb6f9828
Narangerel Bumnasan <narangerel@Naraas-MacBook-Pro.local>
2025-01-28 06:16:24 +0800

    first commit

---
+++ src/app/businesscard/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./global.css";
+
+export default function Home() {
+  return (
+<div id="body">
+<div id="cardContainer">
+
+</div>
+</div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -3,93 +3,47 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div id="body">
+      <div id="cardContainer">
+        <div id="blackSide">
+<div id="briefInfo">
+<img src="Logo000 1.svg" id="companyLogo"/>
+<p>Company Name</p>
+<p>Tag Line Goes Here</p>
+</div>
+<img src="QR Code.svg" id="QR"/>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {/* <div id="vectors">
+          <img src="Vector 8.svg"  id="blueVector"></img>
+          <img src="Vector 9.svg"  id="orangeVector"></img>
+          <img src="Vector 10.svg" id="purpleVector"></img>
+        </div> */}
+        <img src="Shape.svg" id="shape" />
+        <div id="whiteSide">
+          <div id="owner">
+            <div id="ownerName">
+              <p id="firstName">John</p>
+              <p id="lastName">Doe</p>
+            </div>
+            <p id="occupation">Designer</p>
+          </div>
+          <div id="informations">
+            <div className="iconWithText">
+              <img src="akar-icons_phone.svg" className="icons" />
+              <p className="infoText">000-123-456-7890</p>
+            </div>
+            <div className="iconWithText">
+              <img src="line-md_email.svg" className="icons" />
+              <p className="infoText">Email@yourdomain.com</p>
+            </div>
+            <div className="iconWithText">
+              <img src="system-uicons_location.svg" className="icons" />
+              <p className="infoText">Your address goes here<br />
+                125 Street, USA</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
